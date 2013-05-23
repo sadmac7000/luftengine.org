3e4af47a5d51febffd545d54ec4be430549fa89c
Casey Dahlin <cdahlin@redhat.com>
2013-05-23 19:34:04 -0400

    Add mailing list subscription
    Signed-off-by: Casey Dahlin <cdahlin@redhat.com>

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from datetime import date
+
+import requests
+from flask import request
+from config import api_key, debug
 
 app = Flask(__name__, static_url_path='')
 
@@ -23,10 +27,19 @@ def develop():
 def inject_year():
     return dict(year=date.today().strftime("%Y"))
 
-@app.route('/devel_subscribe')
+@app.route('/devel_subscribe', methods=["POST"])
 def devel_subscribe():
-    return ""
+    requests.post(
+            'https://api.mailgun.net/v2/lists/devel@lists.luftengine.org/members',
+            auth=('api', api_key),
+            data={'subscribed': True,
+                'address': request.form['email']})
+    return redirect(url_for('devel_subscribed'))
+
+@app.route('/devel_subscribed')
+def devel_subscribed():
+    return render_template("devel_subscribed.html")
 
 if __name__ == "__main__":
-    app.debug = True
+    app.debug = debug
     app.run()
